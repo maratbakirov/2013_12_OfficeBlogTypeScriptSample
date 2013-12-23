d47b5eed10c55bbdd3214fc0e001ba54a62bd4ad
Marat Bakirov <marat.bakirov@readify.net>
2013-12-23 14:41:14 +1100

    missing files

--- TestPeoplePicker/TestPeoplePicker/Scripts/_references.js
+++ TestPeoplePicker/TestPeoplePicker/Scripts/_references.js
@@ -5,3 +5,7 @@
 /// <reference path="~/_layouts/15/SP.Runtime.js" />
 /// <reference path="~/_layouts/15/SP.UI.Dialog.js" />
 /// <reference path="~/_layouts/15/SP.js" />
+
+///<reference path="typings/sharepoint/SharePoint.d.ts" />
+///<reference path="typings/jquery/jquery.d.ts" />
+
